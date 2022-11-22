bdbf0723f2ebb938eb2eb5c286f3f3c3b7ab17e7
PeterFlandin <peterflandin@gmail.com>
2022-11-22 14:14:58 +0100

    Ajout du Submit de commentaire dans blog

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
 const Article = ({ article }) => {
+  const dateFormater = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    return newDate;
+  };
+
   return (
     <div className="article">
       <div className="card-header">
-      <h3>{article.author}</h3>
-      <em>Posté le : {article.date}</em>
-    </div>
-    <p>{article.content}</p>
-    <div className="btn-container">
+        <h3>{article.author}</h3>
+        <em>Posté le : {dateFormater(article.date)}</em>
+      </div>
+      <p>{article.content}</p>
+      <div className="btn-container">
         <button>Edit</button>
         <button>Supprimer</button>
-    </div>
+      </div>
     </div>
   );
 };
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,29 +5,35 @@ import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
-const [blogData, setBlogData] = useState([]);
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, ssetContent] = useState("");
   const [error, setError] = useState(false);
-  
+
   const getData = () => {
-      axios.get("http://localhost:3004/articles")
-      .then((res) => setBlogData(res.data))
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
   };
 
-useEffect(() => {
-
-getData()
-
-}, []);
-
-
+  useEffect(() => {
+    getData();
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      ssetContent("")
+      getData();
     }
   };
 
@@ -38,19 +44,22 @@ getData()
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input type="text" placeholder="Nom" onChange={(e) => setAuthor(e.target.value)} value={author}/>
         <textarea
-        style={{border: error ? "1px solid red" : "1px solid #61dafb"}}
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
-          onChangeCapture={(e) => ssetContent(e.target.value)}
+          onChange={(e) => ssetContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un min de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
       <ul>
-        {blogData.map((article)=> (
-            <Article key={article.id} article={article}/>
-        ))}
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
       </ul>
     </div>
   );
